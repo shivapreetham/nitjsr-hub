022666c5fad8bd3480727aedc13dd2058924958e
shivapreetham <2005shivapreetham@gmail.com>
2025-06-23 23:53:30 +0530

    me at the bengaluru airport terminal 2

--- src/app/(app)/home/page.tsx
+++ src/app/(app)/home/page.tsx
@@ -141,7 +141,6 @@ export default function Home() {
           </div>
         </div>
 
-        {/* Enhanced Mobile Layout */}
         <div className="md:hidden">
           {/* Mobile Action Button */}
           <div className="fixed bottom-6 right-6 z-50">
