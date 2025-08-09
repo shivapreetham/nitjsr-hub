783de33ae699ce0ed7299a3a128c947768427535
shivapreetham <2005shivapreetham@gmail.com>
2025-08-09 18:57:58 +0530

    added error handling and socket io

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,7 +4,7 @@ import Sidebar from '@/components/sidebar/Sidebar';
 import { CurrentUserProvider } from '@/context/CurrentUserProvider';
 import getCurrentUser from '@/app/(shared)/serverActions/getCurrentUser';
 import StreamVideoProvider from '@/context/StreamClientProvider';
-
+import SocketProvider from '@/context/SocketProvider';
 interface RootLayoutProps {
   children: React.ReactNode;
 }
@@ -20,6 +20,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
   return (
     <CurrentUserProvider currentUser={currentUser}>
       <StreamVideoProvider>
+        <SocketProvider>
         <Sidebar>
           <div className="flex flex-col h-screen pb-5">
             {/* <Navbar /> */}
@@ -27,6 +28,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
             <ActiveStatus />
           </div>
         </Sidebar>
+        </SocketProvider>
       </StreamVideoProvider>
     </CurrentUserProvider>
   );
