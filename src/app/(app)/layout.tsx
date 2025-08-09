d2fb32cc4c25deaf051522dc1c65be173b038bd2
shivapreetham <2005shivapreetham@gmail.com>
2025-08-09 18:58:28 +0530

    added error handling and socket io

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,7 +4,7 @@ import Sidebar from '@/components/sidebar/Sidebar';
 import { CurrentUserProvider } from '@/context/CurrentUserProvider';
 import getCurrentUser from '@/app/(shared)/serverActions/getCurrentUser';
 import StreamVideoProvider from '@/context/StreamClientProvider';
-import SocketProvider from '@/context/SocketProvider';
+import {SocketProvider} from '@/context/SocketProvider';
 interface RootLayoutProps {
   children: React.ReactNode;
 }
